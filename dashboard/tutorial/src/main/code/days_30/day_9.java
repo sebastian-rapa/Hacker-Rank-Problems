9fca5a17cade3a55a2ed922db3d79861dea9e95c
Sebastian <sebastian.rapa16@gmail.com>
2020-05-07 18:03:49 +0300

    Day 9 code challenge Recursion

---
+++ dashboard/tutorial/src/main/code/days_30/day_9.java
@@ -0,0 +1,13 @@
+package main.code.days_30;
+
+class day_9 {
+    static int factorial(int n) {
+        return n != 1 ?
+                n * factorial(n - 1) :
+                1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(3));
+    }
+}
